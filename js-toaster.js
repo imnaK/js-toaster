3ad8a13c7b02107ff10ed2016c6761bd4f7573a4
imnaK <mc.yama1000@gmail.com>
2024-06-28 01:57:36 +0200

    feat: rework id and prefix system

--- js-toaster.js
+++ js-toaster.js
@@ -6,10 +6,14 @@ class Toaster {
 
   constructor(parentElement = document.body, options = {}) {
     this.#options = this.#getSanitizedOptions(options);
-    this.#toasterId = this.#getUUIDv4();
+    this.#toasterId = this.#getId(this.#options.toasterPrefix);
     this.#toasts = {};
 
     this.#initToaster(parentElement);
+  }
+
+  #getId(prefix) {
+    return prefix + this.#getUUIDv4();
   }
 
   #getUUIDv4() {
@@ -51,7 +55,7 @@ class Toaster {
 
   #initToaster(parentElement) {
     this.#toasterElement = document.createElement("div");
-    this.#toasterElement.id = this.getToasterElementId();
+    this.#toasterElement.id = this.#toasterId;
     this.#toasterElement.classList.add("toaster");
     parentElement.appendChild(this.#toasterElement);
   }
@@ -72,43 +76,32 @@ class Toaster {
     return this.#toasterElement;
   }
 
-  getToasterElementId() {
-    return this.#options.toasterPrefix + this.#toasterId;
-  }
-
-  getToastElementId(toastId) {
-    if (!this.#toasts.hasOwnProperty(toastId)) return;
-    return this.#options.toastPrefix + toastId;
-  }
-
   addToast(message, timeout = this.#options.defaultTimeout) {
-    const toastId = this.#getUUIDv4();
+    const toastId = this.#getId(this.#options.toastPrefix);
     this.#toasts[toastId] = message;
+
+    let toastElement = document.createElement("div");
+    toastElement.id = toastId;
+    toastElement.classList.add("toast");
+    toastElement.textContent = message;
+    this.#toasterElement.prepend(toastElement);
+
     setTimeout(() => {
       this.removeToast(toastId);
     }, timeout > 0 ? timeout : this.#options.defaultTimeout);
-    this.updateToasts();
   }
 
   removeToast(toastId) {
     if (!this.#toasts.hasOwnProperty(toastId)) return;
-    this.#toasterElement.querySelector("#" + this.getToastElementId(toastId)).remove();
     delete this.#toasts[toastId];
+    this.#removeToastElement(toastId);
   }
 
-  // TODO: only remove/add necessary toasts for better performance
-  updateToasts() {
-    // remove all toasts
-    while (this.#toasterElement.firstChild)
-      this.#toasterElement.removeChild(this.#toasterElement.firstChild);
-
-    // add all toasts
-    for (const [key, value] of Object.entries(this.#toasts)) {
-      let toastElement = document.createElement("div");
-      toastElement.id = this.getToastElementId(key);
-      toastElement.classList.add("toast");
-      toastElement.textContent = value;
-      this.#toasterElement.prepend(toastElement);
+  #removeToastElement(toastId) {
+    try {
+      this.#toasterElement.querySelector("#" + toastId).remove();
+    } catch (error) {
+      console.error("JS-Toaster Error: There is no toast with id", toastId);
     }
   }
 }
